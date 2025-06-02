d9ccf8a5c174c07d7a96a75055b245e7bc7e5ab5
Luan <luansilvf@gmail.com>
2025-06-02 19:46:31 -0300

    exercises

--- chapter_05/exercises/if_boolen.py
+++ chapter_05/exercises/if_boolen.py
@@ -16,7 +16,6 @@ if string1 == string2:
 else:
     print('The strings are not equal')
 
-
 food1 = 'Rice'
 food2 = 'rice'
 if food1.lower() == food2.lower():
@@ -42,5 +41,46 @@ elif 'yellow' in alien_color:
 elif 'red' in alien_color:
     print('You just earned 15 points!')
 
-# 5.7
-person = 2
+print('----------------------')
+# 5.9
+usernames = ['admin', 'julia123', 'caio123', 'james123', 'sofia123']
+for username in usernames:
+    if not usernames:
+        print(f'The list is empty!')
+
+    elif username.lower() == 'admin':
+        print(f'Hello admin, would you like to see a status report?')
+
+    else:
+        print(f'Hello {username}, thank you for logging in again.')
+
+print('----------------------')
+# 5.10
+
+current_users = ['Bjork', 'Miley', 'Arca', 'Admin', 'James']
+
+new_users = ['Bjork', 'Ashley', 'Jannet', 'Miley']
+
+current_user_lower = [user.lower() for user in current_users]
+
+for new in new_users:
+    if new.lower() in current_user_lower:
+        print(f'The username {new} is not available')
+    else:
+        print(f'The username {new} is available!')
+
+print('----------------------')
+# 5.11
+
+numbers = list(range(1, 10))
+for number in numbers:
+    if number == 1:
+        print("1st")
+    elif number == 2:
+        print("2nd")
+    elif number == 3:
+        print("3rd")
+    else:
+        print(f"{number}th")
+
+
